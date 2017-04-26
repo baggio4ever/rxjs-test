581d2b596b5209b070ca375b729cc41a707e9f93
hirayama <hirayama@bp.horizon.co.jp>
2017-04-26 18:10:04 +0900

    RxJSの勉強中

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component,AfterViewInit } from '@angular/core';
+
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/fromEvent';
+import 'rxjs/add/observable/merge';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'app works!';
+
+  messages = [];
+  n=0;
+
+  ngAfterViewInit() {
+    const button = document.querySelector('button');
+    let first = Observable
+      .fromEvent( button,'click' );
+    let second = Observable
+      .fromEvent( button,'dblclick');
+    
+    Observable.merge(first,second)
+      .subscribe( (event:MouseEvent)=>{
+          this.n++;
+          const s = (event.target as HTMLButtonElement).textContent +' is clicked. '+event.type+' '+this.n;
+          console.log( s );
+          this.messages.push( s );
+      });
+
+  }
 }
