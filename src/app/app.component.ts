f77d31fb34b36186346525db6e3cdbaebc7c0396
hirayama <hirayama@bp.horizon.co.jp>
2017-04-27 13:16:37 +0900

    勉強中。ホント不思議 RxJS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,10 @@ import { Component,AfterViewInit } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/observable/fromEvent';
 import 'rxjs/add/observable/merge';
+import 'rxjs/add/observable/of';
+import 'rxjs/add/operator/delay';
+import 'rxjs/add/operator/concat';
+
 
 @Component({
   selector: 'app-root',
@@ -14,9 +18,10 @@ export class AppComponent implements AfterViewInit {
 
   messages = [];
   n=0;
+  numbers;
 
   ngAfterViewInit() {
-    const button = document.querySelector('button');
+    const button = document.querySelector('.xxy');
     let first = Observable
       .fromEvent( button,'click' );
     let second = Observable
@@ -30,5 +35,19 @@ export class AppComponent implements AfterViewInit {
           this.messages.push( s );
       });
 
+    const numB = Observable.of( 99,98,99,100 );
+    this.numbers = Observable.of( 5,4,3,2,1 ).delay(2000).concat(numB.delay(1000));
+  }
+
+  onDoon(){
+    console.info('どーーん');
+    this.messages.push('どーーん');
+
+
+    this.numbers.subscribe(
+      (x)=>{console.log(x);},
+      (error:Error)=>{console.log('Error!');},
+      ()=>{console.log('--------- completed! -------');}
+    )
   }
 }
